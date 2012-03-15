b3bc0828af0a7ddaca02c4c5b0d8972a7b906a24
David Patterson <dj@dj.(none)>
2012-03-15 12:30:26 +0000

    ajax submit on test confirmation

--- app/assets/javascripts/cofirm_appointment.js
+++ app/assets/javascripts/cofirm_appointment.js
@@ -8,3 +8,4 @@
       $("#confirm_appointment_dialog").dialog();
     });
   });
+  
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,18 +1,17 @@
 class BookingsController < PortalController
-
   # GET /bookings
   # GET /bookings.json
   def index
 
     session[:booking_context] = 'index'
 
-    if current_user.is_booking_manager? 
-    #  @bookings = Booking.all
-    # @bookings = Booking.filter(params[:search], [:reference_number, :company_id])
-     @bookings = Booking.includes(:company).filter(params[:search], [:reference_number, "companies.name"])
+    if current_user.is_booking_manager?
+      #  @bookings = Booking.all
+      # @bookings = Booking.filter(params[:search], [:reference_number, :company_id])
+      @bookings = Booking.includes(:company).filter(params[:search], [:reference_number, "companies.name"])
     @main_heading = 'Bookings'
     else
-      @bookings = current_user.company.bookings
+    @bookings = current_user.company.bookings
     @main_heading = "#{current_user.company.name} Bookings"
     end
 
@@ -35,11 +34,11 @@ class BookingsController < PortalController
 
   # Go to booking
   def goto_booking(booking)
-    
+
     site = @booking.site
     slot_day = @booking.slot_day
     slot_time = @booking.slot_time
-    
+
     # don't need to check slot_time because it is optional
     if session[:booking_context] and session[:booking_context] == 'diary' and site and slot_day
       # Go the diary page
@@ -48,7 +47,7 @@ class BookingsController < PortalController
       end
     else
 
-      # Go to booking edit page
+    # Go to booking edit page
       respond_to do |format|
         format.html { redirect_to edit_booking_path(@booking) }
       end
@@ -59,14 +58,14 @@ class BookingsController < PortalController
   def create
     @booking = Booking.new(params[:booking])
     unless @booking and @booking.slot_time and @booking.slot_time.number_of_free_slots >= @booking.slots_taken_up
-      flash[:notice] = 'No longer enough slots available'
-      redirect_to :back
-      return
+    flash[:notice] = 'No longer enough slots available'
+    redirect_to :back
+    return
     end
 
     if @booking.save
-      flash[:notice] = 'Booking was successfully created'
-      goto_booking(@booking)
+    flash[:notice] = 'Booking was successfully created'
+    goto_booking(@booking)
     else
       @main_heading = 'Review New Booking'
       respond_to do |format|
@@ -80,11 +79,16 @@ class BookingsController < PortalController
     @booking = Booking.find(params[:id])
     if @booking.update_attributes(params[:booking])
       flash[:notice] = 'Booking was successfully updated'
-      goto_booking(@booking)
+    #  goto_booking(@booking)
+      respond_to do |format|
+        format.html
+        
+      end
     else
       @main_heading = 'Review Booking'
       respond_to do |format|
         format.html { render action: "edit" }
+        
       end
     end
   end
@@ -100,17 +104,23 @@ class BookingsController < PortalController
       @booking.update_slot!
       success = true
     rescue => e
-      flash[:error] = e.message
-      success = false
+       flash[:error] = e.message
+       success = false
     end
 
-    if success
-      flash[:notice] = 'Booking confirmed'
-      respond_to do |format|
-        format.html { redirect_to booking_path(@booking) }
-      end      
-    else
-      redirect_to :back
+    respond_to do |format|
+      format.html do
+        if success
+          flash[:notice] = 'Booking confirmed'
+        else
+          redirect_to :back
+        end
+        redirect_to booking_path(@booking)
+      end
+      format.js do
+        # do the update here
+        render 'update'
+      end
     end
   end
 
@@ -120,28 +130,28 @@ class BookingsController < PortalController
 
     if @booking
       if session[:booking_context] and session[:booking_context] == 'diary'
-        site = @booking.site
-        slot_day = @booking.slot_day
-        slot_time = @booking.slot_time
+      site = @booking.site
+      slot_day = @booking.slot_day
+      slot_time = @booking.slot_time
       end
 
       if @booking.destroy
-        flash[:notice] = 'booking removed'
+      flash[:notice] = 'booking removed'
       else
-        flash[:error] = 'could not remove booking'
+      flash[:error] = 'could not remove booking'
       end
     end
 
     # don't need to check slot_time; that's optional
     if site and slot_day
-      
+
       # redirect to diary page
       respond_to do |format|
         format.html { redirect_to diary_slot_day_time_path(site, slot_day, slot_time) }
       end
     else
 
-      # redirect to booking index
+    # redirect to booking index
       respond_to do |format|
         format.html { redirect_to bookings_url }
       end
